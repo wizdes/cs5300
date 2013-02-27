cd345de49a9958923e3a83f2f234881221f57659
ms2786 <ms2786@cornell.edu>
2013-02-27 00:13:27 -0500

    gc fails

---
+++ com_app/src/coreservlets/GarbageCollectorThread.java
@@ -0,0 +1,9 @@
+package coreservlets;
+
+import java.util.concurrent.ConcurrentMap;
+
+public class GarbageCollectorThread extends Thread{
+	public GarbageCollectorThread(ConcurrentMap<Integer, UserContents> session_info){
+		
+	}
+}
