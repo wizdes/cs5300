fc73337c40bc365aa9ddbf936b958ffe17ee9e40
Patrick Berens <pcb73@gmail.com>
2013-03-07 16:38:43 -0500

    Basic rpc marshalling template.

---
+++ cs5300p1/src/rpc_layer/Marshalling.java
@@ -0,0 +1,22 @@
+package rpc_layer;
+
+public class Marshalling {
+
+	/**
+	 * RPC marshalling function.
+	 * @param objs Objects which properlly implement toString. (ex: Integer, String, Double, custom class, etc)
+	 * @return Byte array containing objects encoded with UTF-8 and Base64.
+	 */
+	public byte[] marshall(Object[] objs){
+		return null;
+	}
+	/**
+	 * RPC demarshalling function.
+	 * @param bytes Byte array containing objects encoded with UTF-8 and Base64.
+	 * @return Objects which properlly implement toString. (ex: Integer, String, Double, custom class, etc)
+	 */
+	public Object[] unmarshall(byte[] bytes){
+		//This may return String[] instead.
+		return null;
+	}
+}
