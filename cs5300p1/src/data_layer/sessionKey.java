72ea7a57724829467e6138bb4565dbd77486de0d
OpenShift guest <yl2326@cornell.edu>
2013-03-25 14:04:51 -0400

    made the sessionKey work in a hash table

--- cs5300p1/src/data_layer/sessionKey.java
+++ cs5300p1/src/data_layer/sessionKey.java
@@ -25,5 +25,21 @@ public class sessionKey {
 	public void setVersionNumber(int versionNumber){
 		this.versionNumber = versionNumber;
 	}
+    
+    public int hashCode(){
+        String hashString = sessionID + "_" + Integer.toString(versionNumber);
+        return hashString.hashCode();
+    }
+    
+    public boolean equals(Object obj){
+        if(!(obj instanceof sessionKey)){
+            return false;
+        }
+        sessionKey other = (sessionKey) obj;
+        if(sessionID.equals(other.sessionID) && versionNumber == other.versionNumber){
+            return true;
+        }
+        return false;
+    }
 	
 }
